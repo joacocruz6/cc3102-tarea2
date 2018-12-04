200867ad11739e0133ad5bd5c19a99054075a103
Joaquin Cruz <joaquin.cruz@ug.uchile.cl>
2018-12-03 21:07:38 -0300

    Start to debugging some stuff

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ import ply.yacc as yacc
 #simbolo entero
 tokens=[
     "INT",
-    "TRUE",
-    "FALSE",
     "PLUS",
     "NAME",
     "MINUS",
@@ -93,15 +91,7 @@ def t_INT(t):
     r'\d+'
     t.value=int(t.value)
     return t
-#definicion de los booleanos
-def t_TRUE(t):
-    r'true'
-    t.value=True
-    return t
-def t_FALSE(t):
-    r'false'
-    t.value=False
-    return t
+#debemos
 #definicion de las variables
 def t_NAME(t):
     r'[a-z][a-z0-9]*'
@@ -118,6 +108,7 @@ def t_error(t):
 precedence=(
     ('left','PLUS','MINUS'),
     ('left','MULTIPLY','DIVIDE')
+    ('right','UMINUS')
 )
 def p_empty(p):
     '''
@@ -146,21 +137,22 @@ def p_variables(p):
     '''
     variables : NAME ASSIGN  expression
     '''
-    p[0]=p[1]
+    p[0]=('=',p[1],p[3])
+
 #comparaciones y loops
 def p_conditional(p):
     '''
-    conditional : IF LEFTBRACKET booleanexpression RIGHTBRACKET THEN WLEFTBRACKET program WRIGHTBRACKET
+    conditional : IF LEFTBRACKET expression RIGHTBRACKET THEN WLEFTBRACKET program WRIGHTBRACKET
     '''
     p[0]=('if',p[3],p[7]) #todo revisar esto, no se si esta bien
 def p_conditional_else(p):
     '''
-    conditional : IF LEFTBRACKET booleanexpressionRIGHTBRACKET THEN WLEFTBRACKET program WRIGHTBRACKET ELSE WLEFTBRACKET program WRIGHTBRACKET
+    conditional : IF LEFTBRACKET expression RIGHTBRACKET THEN WLEFTBRACKET program WRIGHTBRACKET ELSE WLEFTBRACKET program WRIGHTBRACKET
     '''
     p[0]=('ifelse',p[3],p[7],p[11]) #todo revisar
 def p_loop(p):
     '''
-    loop : WHILE LEFTBRACKET booleanexpression RIGHTBRACKET DO WLEFTBRACKET program WRIGHTBRACKET
+    loop : WHILE LEFTBRACKET expression RIGHTBRACKET DO WLEFTBRACKET program WRIGHTBRACKET
     '''
     p[0]=('while',p[3],p[7])
 #lectura y impresiones en pantalla
@@ -175,9 +167,94 @@ def p_input(p):
     '''
     p[0]=('read',p[1])
 #expresiones
-#todo, las expresiones, que pueden ser booleanos o normales. Los booleanos pueden ser comparativos o true o false. Los otros como calc.
+#simples:
+def p_expression_number(p):
+    '''
+    expression : INT
+    '''
+    p[0]=p[1]
+def p_expression_arithmetic(p):
+    '''
+    expression : expression PLUS expression
+               | expression MINUS expression
+               | expression MULTIPLY expression
+               | expression DIVIDE expression
+               | expression EQUALS expression
+               | expression LOWER expression
+               | expression GREATER expression
+               | expression NOTEQ expression
+               | expression LOWEREQ expression
+               | expression GREATEREQ expression
+    '''
+    p[0]=(p[2],p[1],p[3])
 
-
+def p_expression_parenthesis(p):
+    '''
+    expression : LEFTBRACKET expression RIGHTBRACKET
+    '''
+    p[0]=p[2]
+def p_expression_minus(p):
+    '''
+    expression : MINUS expression
+    '''
+    p[0]=-p[2]
+def p_expression_var(p):
+    '''
+    expression : NAME
+    '''
+    p[0]=('var',p[1])
+#funcion de errores
+def p_error(p):
+    print("Segmentation Fault") #o Syntax Error pero no corresponde el meme
+#funcion evaluadora
+env_var={}
+def evaluate(p):
+    global env_var
+    if type(p)==tuple:
+        if p[0]=='+':
+            return run(p[1])+run(p[2])
+        if p[0]=='-':
+            return run(p[1])-run(p[2])
+        if p[0]=='*':
+            return run(p[1])*run(p[2])
+        if p[0]=='/':
+            return run(p[1])/run(p[2])
+        if p[0]=='==':
+            return run(p[1])==run(p[2])
+        if p[0]=='<':
+            return run(p[1])<run(p[2])
+        if p[0]=='>':
+            return run(p[1])>run(p[2])
+        if p[0]=='<=':
+            return run(p[1])<=run(p[2])
+        if p[0]=='>=':
+            return run(p[1])>=run(p[2])
+        if p[0]=='=':
+            env_var[p[1]]=run(p[2])
+        if p[0]=='var':
+            if p[1] not in env_var:
+                return "Variable have not been assigned yet"
+            else:
+                return env_var[p[1]]
+        if p[0]=='if':
+            cond=int(run(p[1]))!=0
+            if cond:
+                run(p[7])
+        if p[0]=='ifelse':
+            cond=int(run(p[1]))!=0
+            if cond:
+                run(p[2])
+            else:
+                run(p[3])
+        if p[0]=='while':
+            while run(p[1])!=0:
+                run(p[2])
+        if p[0]=='read':
+            p[1]=input()
+        if p[0]=='print':
+            print(p[1])
+    else:
+        return p
 #Un pequeño ejemplo
 def main():
     lexer=lex.lex()
